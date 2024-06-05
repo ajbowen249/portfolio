72fd9bc373b643d466963e322e79373af2089a71
Alex Bowen <ajbowen249@gmail.com>
2024-06-05 15:21:03 -0400

    refactor: reworking columnation

--- src/components/BrandedLink.vue
+++ src/components/BrandedLink.vue
@@ -1,6 +1,5 @@
 <template>
     <a class="branded-link" :href="href">
-        <div v-if="iconName == 'npm'" class="brand-icon-background"></div>
         <component v-if="IconMap[iconName]" :is="IconMap[iconName]" class="brand-icon" />
         <span class="branded-link-text">{{ text }}</span>
     </a>
@@ -28,19 +27,15 @@ defineProps<{
 <style scoped>
 .branded-link {
     margin-left: var(--space-xxs);
-    display: grid;
+    display: flex;
     height: var(--space-l);
     margin-bottom: var(--space-m);
     align-content: center;
     color: var(--text-color);
     font-size: 20pt;
-    grid-template-rows: 1fr;
-    grid-template-columns: auto 1fr;
 }
 
 .brand-icon {
-    grid-row: 1;
-    grid-column: 1;
     margin-top: 10px;
     margin-left: var(--space-xxs);
     margin-right: var(--space-xxs);
@@ -49,18 +44,7 @@ defineProps<{
 }
 
 .branded-link-text {
-    grid-row: 1;
-    grid-column: 2;
     margin-left: var(--space-xxs);
-}
-
-.brand-icon-background {
-    grid-row: 1;
-    grid-column: 1;
-    height: 20px;
-    margin-top: 15px;
-    background-color: white;
-    border-radius: 5px;
 }
 
 @media only screen and (max-width: 900px) {
@@ -76,10 +60,6 @@ defineProps<{
 
     .branded-link-text {
         margin-top: var(--space-xxs);
-    }
-
-    .brand-icon-background {
-        height: 15px;
     }
 }
 </style>
